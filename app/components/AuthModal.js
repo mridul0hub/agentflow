6837bceabb9780f4bf5a04afd6743d7b6ee52a34
Mridul Soni <mridulsoni08@gmail.com>
2026-03-12 22:43:28 +0530

    logo change

--- app/components/AuthModal.js
+++ app/components/AuthModal.js
@@ -236,7 +236,7 @@ export default function AuthModal({ isOpen, onClose, defaultTab = "login" }) {
             <button className="am-close" onClick={onClose}>✕</button>
 
             <div className="am-brand">
-              <div className="am-brand-icon">✦</div>
+              <img src="/logo.png" style={{ height: "30px", width: "30px", borderRadius: "8px" }} />
               <span className="am-brand-name">Soni AI Agents</span>
             </div>
 
